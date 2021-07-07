c9f995ffc1af710aa1258e0f06e8a8339aec87e5
andreagavardi <andreagavardi89@gmail.com>
2021-07-07 15:40:25 +0200

    Init progetto e stampa Hello World a schermo

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $data = [
+        'title' => 'studenti classe 35',
+        'subtitle' => 'Imparando Laravel',
+        'students' => [
+            'Anna',
+            'Giacomo',
+            'Andrei',
+            'Andrea',
+            'Emanuele',
+            'Beatrice',
+            'Roberto',
+            'Laura'
+        ],
+    ];
+
+    return view('home', $data);
+});
